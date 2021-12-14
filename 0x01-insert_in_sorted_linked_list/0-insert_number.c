9e20108d7d07a62755a54a1d2ba3a7eb31e7ed39
faykris <faykris28@gmail.com>
2021-12-14 17:28:51 -0500

    [Add] list.h 0-insert_number.c: pending betty code

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * *insert_node -  inserts a number into a sorted singly linked list.
+ * @head: double pointer to head of list
+ * @number: number to be added into a sorted singly linked list.
+ *
+ * Return: Address of the new node, on error retrieves NULL
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new = NULL;
+	listint_t *tmp = NULL;
+
+	if (head == NULL)
+		return (NULL);
+	tmp = *head;
+	new = malloc(sizeof(listint_t));
+	new->n = number;
+	new->next = NULL;
+	if (new == NULL)
+		return (NULL);
+	if (tmp->n >= number)
+	{
+		new->next = tmp;
+		*head = new;
+		return (new);
+	}
+	while (tmp->next != NULL)
+	{
+		if (tmp->next->n >= number)
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
+		tmp = tmp->next;
+	}
+	tmp->next = new;
+	return (new);
+}
---
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -0,0 +1,2 @@
+# 0x01. Insert in sorted linked list
+This folder contains the files required to a solve task in this project.
