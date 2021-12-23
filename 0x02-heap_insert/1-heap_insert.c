1de55d05069ef727fc77e0cbc47c1a1e69882ea3
faykris <faykris28@gmail.com>
2021-12-22 20:07:30 -0500

    [Update]1-heap_insert.c: implement swap values

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,5 +1,63 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_root - checks if a given node is a root
+ * @node: node tree to check
+ *
+ * Return: 1 if node is a root, otherwise 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	if (node->parent == NULL)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: node tree to check
+ *
+ * Return: 1 if node is a leaf, otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * swap - change node values
+ * @node1: node 1 tree to change
+ * @node2: node 2 tree to change
+ *
+ * Return: new position of node greater
+ */
+heap_t *swap(heap_t *node1, heap_t *node2)
+{
+	int num = node1->n;
+
+	node1->n = node2->n;
+	node2->n = num;
+
+	return (node2);
+}
 
 /**
  * heap_insert - Inserts a value into a Max Binary Heap
@@ -11,35 +69,22 @@
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new;
+	heap_t *temp;
 
 	if (!*root)
-	{
-		return (*root = binary_tree_node(NULL, value));
-	}
+		temp = (*root = binary_tree_node(*root, value));
+	else if (!(*root)->left)
+		temp = ((*root)->left = binary_tree_node(*root, value));
+	else if (!(*root)->right)
+		temp = ((*root)->right = binary_tree_node(*root, value));
 	else
 	{
-		if (value >= (*root)->n)
-		{
-			new = binary_tree_node(NULL, value);
-			(*root)->parent = new;
-			if (!new->left)
-				new->left = *root;
-			else if (!new->right)
-				new->right = *root;
-			*root = new;
-		}
-		else
-		{
-			new = binary_tree_node(*root, value);
-			new->parent = *root;
-			if (!(*root)->left)
-				(*root)->left = new;
-			else if (!(*root)->right)
-				(*root)->right = new;
-		}
-		return (new);
+		temp = *root;
+		temp = heap_insert(&temp->left, value);
+		if (!temp)
+			temp = heap_insert(&temp->right, value);
 	}
-
-	return (binary_tree_node(*root, value));
+	if (temp->parent && temp->n > temp->parent->n)
+		temp = swap(temp, temp->parent);
+	return (temp);
 }
