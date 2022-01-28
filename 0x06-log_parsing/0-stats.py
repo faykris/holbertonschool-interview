410663a77a30a17833955d1b011cc9bb2e740998
faykris <faykris28@gmail.com>
2022-01-28 02:30:50 -0500

    [Add] 0-stats.py: trying add if not imported module

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,44 +1,44 @@
 #!/usr/bin/python3
 """script that reads stdin line by line and computes metrics"""
 
+if __name__ == "__main__":
+    import sys
 
-import sys
+    count = 0
+    fileSize = 0
+    statCount = {"200": 0, "301": 0, "400": 0, "401": 0,
+                 "403": 0, "404": 0, "405": 0, "500": 0}
 
-count = 0
-fileSize = 0
-statCount = {"200": 0, "301": 0, "400": 0, "401": 0,
-             "403": 0, "404": 0, "405": 0, "500": 0}
+    try:
+        for line in sys.stdin:
+            count += 1
+            split = line.split(" ")
+            try:
+                status = split[-2]
+                fileSize += int(split[-1])
+                if status in statCount:
+                    statCount[status] += 1
+            except Exception:
+                pass
 
-try:
-    for line in sys.stdin:
-        count += 1
-        split = line.split(" ")
-        try:
-            status = split[-2]
-            fileSize += int(split[-1])
-            if status in statCount:
-                statCount[status] += 1
-        except Exception:
-            pass
+            if count % 10 == 0:
+                print("File size: {}".format(fileSize))
+                for key in sorted(statCount.keys()):
+                    if statCount[key] == 0:
+                        continue
+                    print("{}: {}".format(key, statCount[key]))
 
-        if count % 10 == 0:
+        else:
             print("File size: {}".format(fileSize))
             for key in sorted(statCount.keys()):
                 if statCount[key] == 0:
                     continue
                 print("{}: {}".format(key, statCount[key]))
 
-    else:
+    except (KeyboardInterrupt, SystemExit):
         print("File size: {}".format(fileSize))
         for key in sorted(statCount.keys()):
             if statCount[key] == 0:
                 continue
             print("{}: {}".format(key, statCount[key]))
-
-except (KeyboardInterrupt, SystemExit):
-    print("File size: {}".format(fileSize))
-    for key in sorted(statCount.keys()):
-        if statCount[key] == 0:
-            continue
-        print("{}: {}".format(key, statCount[key]))
-    raise
+        raise
