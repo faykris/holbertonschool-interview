f58cd936b055d4587b78b4d383b861e7fa668ed1
faykris <faykris28@gmail.com>
2022-02-09 15:14:39 -0500

    [Add]0-is_palindrome.c: validate with checker

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,30 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - checks if a unsigned integer is a palindrome or not
+ *
+ * @n: long number to be checked
+ *
+ * Return: 1 if n is a palindrome, and 0 otherwise
+ */
+int is_palindrome(unsigned long n)
+{
+	char text[20];
+	size_t i = 0, j = 0, is_palindrome = 1;
+
+	sprintf(text, "%ld", n);
+	j = strlen(text) - 1;
+
+	while (i < strlen(text) / 2)
+	{
+		if (text[i] != text[j])
+		{
+			is_palindrome = 0;
+			break;
+		}
+		i++;
+		j--;
+	}
+
+	return (is_palindrome);
+}
---
+++ 0x08-palindrome_integer/README.md
@@ -0,0 +1,2 @@
+# 0x08. Palindrome integer
+This directory contains the task file with the solution for palindrome task project
