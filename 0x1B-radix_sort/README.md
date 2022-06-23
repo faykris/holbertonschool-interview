3a82a067ff2a8fd53998611f2ee041f145beeeff
faykris <faykris28@gmail.com>
2022-06-23 18:12:53 -0500

    [Add] all mandatory task: verify with checker

---
+++ 0x1B-radix_sort/README.md
@@ -0,0 +1,2 @@
+# 0x1B-radix_sort
+This directory contains the required files to solve this unique task.
---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+/* standard libraries */
+#include <stddef.h>
+#include <stdlib.h>
+
+/* prototype functions */
+void print_array(const int *array, size_t size);
+void radix_sort(int *array, size_t size);
+
+#endif /* SORT_H */
