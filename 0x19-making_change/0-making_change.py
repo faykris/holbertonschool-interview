1b3580862726344b445d9276f36a2122ab431b90
faykris <faykris28@gmail.com>
2022-06-24 12:00:02 -0500

    [Update] 0-making_change.py: verify with checker

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -5,23 +5,19 @@
 def makeChange(coins, total):
     """makeChange - function"""
 
-    change = 0
-    sw = True
-    if(total <= 0):
+    if total <= 0:
         return 0
-    else:
-        while sw is True:
-            save = change
-            for coin in sorted(coins, reverse=True):
-                if total >= coin:
-                    if total % coin == 0:
-                        change = int(change + total / coin)
-                        sw = False
-                        break
-                    else:
-                        change = int(change + total / coin)
-                        total = total % coin
-                        break
-            if change is save:
-                return -1
-    return int(change)
+    if len(coins) == 0:
+        return -1
+    coins = sorted(coins)
+    dynamic = [float('inf')] * (total + 1)
+    dynamic[0] = 0
+    for i in range(total + 1):
+        for coin in coins:
+            if coin > i:
+                break
+            if dynamic[i - coin] != -1:
+                dynamic[i] = min(dynamic[i - coin] + 1, dynamic[i])
+    if dynamic[total] == float('inf'):
+        return -1
+    return dynamic[total]
