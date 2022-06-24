7c5e1942bec6b00706b5ee9ee5b743e04eae561c
faykris <faykris28@gmail.com>
2022-06-24 12:05:48 -0500

    [Update] 0-making_change.py: verify with checker

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -5,21 +5,16 @@
 def makeChange(coins, total):
     """makeChange - function"""
 
-    if total <= 0:
+    sum = 0
+    if (total <= 0):
         return 0
-
     coins.sort(reverse=True)
-
-    i, ncoins = (0, 0)
-    cpy_total = total
-    len_coins = len(coins)
-
-    while(i < len_coins and cpy_total > 0):
-        if (cpy_total - coins[i]) >= 0:
-            cpy_total -= coins[i]
-            ncoins += 1
-        else:
-            i += 1
-
-    check = cpy_total > 0 and ncoins > 0
-    return -1 if check or ncoins == 0 else ncoins
+    for i in coins:
+        if (total < i):
+            pass
+        q, r = divmod(total, i)
+        total = r
+        sum += q
+    if (total != 0):
+        return -1
+    return sum
