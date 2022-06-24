b584859e8c93bd3193cdbed82ed48524f023c336
faykris <faykris28@gmail.com>
2022-06-24 12:03:47 -0500

    [Update] 0-making_change.py: verify with checker

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -7,17 +7,19 @@ def makeChange(coins, total):
 
     if total <= 0:
         return 0
-    if len(coins) == 0:
-        return -1
-    coins = sorted(coins)
-    dynamic = [float('inf')] * (total + 1)
-    dynamic[0] = 0
-    for i in range(total + 1):
-        for coin in coins:
-            if coin > i:
-                break
-            if dynamic[i - coin] != -1:
-                dynamic[i] = min(dynamic[i - coin] + 1, dynamic[i])
-    if dynamic[total] == float('inf'):
-        return -1
-    return dynamic[total]
+
+    coins.sort(reverse=True)
+
+    i, ncoins = (0, 0)
+    cpy_total = total
+    len_coins = len(coins)
+
+    while(i < len_coins and cpy_total > 0):
+        if (cpy_total - coins[i]) >= 0:
+            cpy_total -= coins[i]
+            ncoins += 1
+        else:
+            i += 1
+
+    check = cpy_total > 0 and ncoins > 0
+    return -1 if check or ncoins == 0 else ncoins
