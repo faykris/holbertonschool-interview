42de5d8acd833a568e9cc0d2786c6fc088395a13
faykris <faykris28@gmail.com>
2022-06-24 12:12:35 -0500

    [Update] 0-making_change.py: verify with checker

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -8,18 +8,16 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    if (coins is None or len(coins) == 0):
-        return -1
-
-    change = 0
-    my_coins = sorted(coins, reverse=True)
-    money_left = total
-
-    for coin in my_coins:
-        while (money_left % coin >= 0 and money_left >= coin):
-            change += int(money_left / coin)
-            money_left = money_left % coin
-
-    change = change if money_left == 0 else -1
-
-    return change
+    coins.sort(reverse=True)
+    stack = total
+    mv = 0
+    cnt = 0
+    while (mv < len(coins)):
+        if stack == 0:
+            return cnt
+        if coins[mv] > stack:
+            mv += 1
+        else:
+            stack -= coins[mv]
+            cnt += 1
+    return -1
