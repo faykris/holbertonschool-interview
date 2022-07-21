3443466aed22eacae9203083837522232387a125
faykris <faykris28@gmail.com>
2022-07-20 19:08:29 -0500

    [Update] 0-binary_tree_is_avl.c: Fix Betty style

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -11,17 +11,17 @@
  */
 int validate_is_avl(const binary_tree_t *tree, int min, int max, int *height)
 {
-    int h1 = 0, h2 = 0;
+	int h1 = 0, h2 = 0;
 
-    if (!tree)
-        return (1);
-    if (tree->n <= min || tree->n >= max)
-        return (0);
-    if (!validate_is_avl(tree->left, min, tree->n, &h1) ||
-        !validate_is_avl(tree->right, tree->n, max, &h2))
-        return (0);
-    *height = MAX(h1, h2) + 1;
-    return (ABS(h1 - h2) <= 1);
+	if (!tree)
+		return (1);
+	if (tree->n <= min || tree->n >= max)
+		return (0);
+	if (!validate_is_avl(tree->left, min, tree->n, &h1) ||
+		!validate_is_avl(tree->right, tree->n, max, &h2))
+		return (0);
+	*height = MAX(h1, h2) + 1;
+	return (ABS(h1 - h2) <= 1);
 }
 
 /**
@@ -33,9 +33,9 @@ int validate_is_avl(const binary_tree_t *tree, int min, int max, int *height)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    int height;
+	int height;
 
-    if (!tree)
-        return (0);
-    return (validate_is_avl(tree, INT_MIN, INT_MAX, &height));
+	if (!tree)
+		return (0);
+	return (validate_is_avl(tree, INT_MIN, INT_MAX, &height));
 }
